0cf321cda64ee63f8f14caa5a6aa2b5fe35919ec
Hery <153767421+Hery0019@users.noreply.github.com>
2024-11-15 21:41:29 +0300

    Update CTRL_annonce.php

--- application/controllers/CTRL_annonce.php
+++ application/controllers/CTRL_annonce.php
@@ -3,7 +3,13 @@ defined('BASEPATH') OR exit('No redirect script access allowed');
 
 class CTRL_annonce extends CI_Controller {
     public function __construct() {
-        
+        parent::__construct();
+    }
+
+    public index() {
+        $this->load->model('Annonce');
+        $data['competence'] = $this->Annonce->get_comptence(); 
+        $this->load->view('form_page', $data);
     }
 }
 
