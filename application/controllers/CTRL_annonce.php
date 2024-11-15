224666fe50369c95bab36527ea0f86544ab64496
Hery <153767421+Hery0019@users.noreply.github.com>
2024-11-15 20:55:58 +0300

    Create CTRL_annonce.php

---
+++ application/controllers/CTRL_annonce.php
@@ -0,0 +1,10 @@
+<?php
+defined('BASEPATH') OR exit('No redirect script access allowed');
+
+class CTRL_annonce extends CI_Controller {
+    public function __construct() {
+        
+    }
+}
+
+?>
